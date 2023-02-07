df8b52c7271a15ee959f811965c5d653863debb1
diaconu96vi <diaconu.ionut96@gmail.com>
2023-02-07 20:06:41 +0100

    Add variables

--- home.js
+++ home.js
@@ -1,4 +1,10 @@
-showMessage("Changes...");
+let welcome = 'Hello World';
+let price = 49.99;
+let name = 'Hiking Boots';
+let discounted = false;
+
+
+showMessage(discounted);
 
 console.log("log");
 // one line comment 
